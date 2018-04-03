b8c7027720aec841a69c4665095a9281f86fce73
sunjingc <1720663962@qq.com>
2018-04-03 11:19:33 +0800

    Add files via upload

--- MoveForwardDemo/src/moveBackward.cpp
+++ MoveForwardDemo/src/moveBackward.cpp
@@ -1,26 +1,26 @@
 /*
- * moveForward.cpp
+ * moveBackward.cpp
  *
  *  Created on: 2018年4月3日
  *      Author: mm
  */
-
-#include "GPIOlib.h"
+#include <GPIOlib.h>
 
 using namespace GPIO;
 
 int main(){
-	//初始化
+	//init
 	init();
 
-	//前进
-	controlLeft(FORWARD,10);
-	controlRight(FORWARD,10);
+	//move backward
+	controlLeft(BACKWARD,20);
+	controlRight(BACKWARD,20);
 	delay(1000);
 
-	//停止
+	//stop
 	stopLeft();
 	stopRight();
 
 	return 0;
 }
+
