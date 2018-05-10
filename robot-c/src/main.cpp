94da390a0be2b93aee5e98f9cc4dbab8c5e20b69
sunjingc <1720663962@qq.com>
2018-05-10 13:24:37 +0800

    modify the main.cpp
    Signed-off-by: sunjingc <1720663962@qq.com>

--- robot-c/src/main.cpp
+++ robot-c/src/main.cpp
@@ -10,8 +10,8 @@
 #include <string>
 #include "GPIOlib.h"
 
-using namespace std;
 using namespace GPIO;
+using namespace std;
 
 int moveForward(int speed,int milliseconds){
 	init();
@@ -39,9 +39,10 @@ int moveBackward(int speed,int milliseconds){
 	return 0;
 }
 
-int stop(){
+int stop(int milliseconds){
 	stopLeft();
 	stopRight();
+	delay(milliseconds);
 
 	return 0;
 }
@@ -62,7 +63,7 @@ int turnRight(int angle){
 
 int main(){
 	ifstream in;
-	string filename = "/dev/rfcomm0";
+	string filename = "rfcomm0";
 	in.open(filename);
 
 	if(!in){
@@ -71,15 +72,57 @@ int main(){
 	}
 
 	char ch;
+	int counter = 0;
 	int flag = 0;
-	const int speed = 50;
+	const int speed = 10;
 	int angle = 0;
 	int length = 0;
+	int second = 0;
 	int milliseconds = 0;
 
 	while(!in.eof()){
 		string str = "";
 		in.read(&ch, 1);
+		while(ch!=',' && ch!='}' && ch!='{' && ch!='"' && ch!=':'){//将每个指令分开
+				while(ch>='0'&&ch<='9'){
+					str = str + ch;
+					in.read(&ch, 1);
+				}
+				if(counter == 0){
+					flag = stoi(str);
+					str = "";
+					counter = 1;
+				}else if(counter == 1){
+					switch(flag){
+					case 0:
+						length = stoi(str);
+						milliseconds = length*100;
+						moveBackward(speed,milliseconds);
+						break;
+					case 1:
+						length = stoi(str);
+						milliseconds = length*100;
+						moveForward(speed,milliseconds);
+						break;
+					case 2:
+						angle = stoi(str);
+						turnRight(angle);
+						break;
+					case 3:
+						angle = stoi(str);
+						turnLeft(angle);
+						break;
+					case 4:
+						second = stoi(str);
+						milliseconds = second*1000;
+						stop(milliseconds);
+						break;
+					}
+					str = "";
+					counter = 0;
+				}
+			in.read(&ch, 1);
+		}
 	}
 	return 0;
 }
